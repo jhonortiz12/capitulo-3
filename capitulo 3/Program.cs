c9cad8df797b1026d7b38bb93545e22cc85f167a
jhon <jhon@DESKTOP-9T4EFST>
2022-11-13 20:58:44 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ejercicio3
+{
+    public class   program
+    {
+        public static void Main()
+        {
+            double factura, descuento, precio, preciobruto, ivacompra;
+            int cantidadproduct;
+            Console.WriteLine("ingrese el precio del producto");
+            precio = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la cantidad que va a llevar");
+            cantidadproduct = int.Parse(Console.ReadLine());
+            ivacompra = (precio * cantidadproduct) * 0.12;
+            Console.WriteLine("el iva de un valor es de " + ivacompra);
+            preciobruto = (precio * cantidadproduct) * ivacompra;
+            if (preciobruto > 50000)
+            {
+                descuento = preciobruto * 0.05;
+                factura = preciobruto - descuento;
+
+
+
+
+
+
+
+
+            }
+            else
+            {
+                factura = preciobruto;
+
+           
+            
+            }
+
+            Console.WriteLine("el precio de la factura es de ");
+        }    
+    }
+}
